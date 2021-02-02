a60cce7bb83ccd6f14bd3350f72cdd993df2eb31
Paul Jewell <paul@teulu.org>
2021-02-02 19:46:06 +0000

    Added full paths for target files

--- update-pkg.sh
+++ update-pkg.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh 
+
+suckless_root="/home/paul/projects/guix/pj-suckless"
 for repo in paulj-dmenu paulj-dwm paulj-slock paulj-st 
     do 	
         git_hash=$(git -C ~/git/$repo log -n 1 --format=format:"%H") 	
         guix_hash=$(guix hash --hash=sha256 --format=nix-base32 -rx ~/git/$repo) 	
         echo $repo $git_hash $guix_hash 	
-        sed -i "s/(commit \".\+\")/(commit \"$git_hash\")/g" paulj/packages/$repo.scm 	
-        sed -i "s/(base32 \".\+\")/(base32 \"$guix_hash\")/g" paulj/packages/$repo.scm 
+        sed -i "s/(commit \".\+\")/(commit \"$git_hash\")/g" $suckless_root/paulj/packages/$repo.scm 	
+        sed -i "s/(base32 \".\+\")/(base32 \"$guix_hash\")/g" $suckless_root/paulj/packages/$repo.scm 
     done
 
