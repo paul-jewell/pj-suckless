126570e07243b967a3b1577cd9e66d674a4d5489
Paul Jewell <paul@teulu.org>
2021-02-22 14:49:38 +0000

    Corrected typo

--- paulj/packages/paulj-dwm.scm
+++ paulj/packages/paulj-dwm.scm
@@ -13,8 +13,8 @@
      (method git-fetch)
      (uri (git-reference
            (url "https://github.com/paul-jewell/paulj-dwm.git")
-           (commit "a9f00066a5fc855c0f65b55075a956dd1ba9ef88")))
+           (commit "3d0de8521a5d625e46c2b6eab22c7b51cc524e6c")))
      (sha256
-      (base32 "16w44zhn5792b6fgx1hl47xsdlbwsy6sk6mvijc1av472i5xc9ij"))))
+      (base32 "13xrqm82xlw0f70gcjdxxjf5qrnfkxnrxnfzsr2d4rlb4varh9jx"))))
    (home-page "https://github.com/paul-jewell/paulj-dwm.git")
    (synopsis "A version of dwm, configured by paulj")))
