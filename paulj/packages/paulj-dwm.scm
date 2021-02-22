26b24aaece60a2579a2864ce444b68ae4e9945ed
Paul Jewell <paul@teulu.org>
2021-02-22 16:58:52 +0000

    Typo correction, plus added grid layout

--- paulj/packages/paulj-dwm.scm
+++ paulj/packages/paulj-dwm.scm
@@ -13,8 +13,8 @@
      (method git-fetch)
      (uri (git-reference
            (url "https://github.com/paul-jewell/paulj-dwm.git")
-           (commit "dda3f16ed99153ee8543598f52f75a911bd11ac2")))
+           (commit "363aa47e6280200d0356473b60889f5192e7f9f7")))
      (sha256
-      (base32 "1bl2075yxdr5c1768pqgc2fp603hny35g0c8g3ks1rxwri3nhy71"))))
+      (base32 "0sxjlfjm6z9blp4db76i0qzxi5r54zsgcp0plbw1sih7f7dbky8i"))))
    (home-page "https://github.com/paul-jewell/paulj-dwm.git")
    (synopsis "A version of dwm, configured by paulj")))
