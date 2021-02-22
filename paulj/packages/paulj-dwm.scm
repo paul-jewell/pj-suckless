fa2746ff80d15f29eb5558188735e133f72ac7f3
Paul Jewell <paul@teulu.org>
2021-02-22 14:41:57 +0000

    Added window swallow

--- paulj/packages/paulj-dwm.scm
+++ paulj/packages/paulj-dwm.scm
@@ -13,8 +13,8 @@
      (method git-fetch)
      (uri (git-reference
            (url "https://github.com/paul-jewell/paulj-dwm.git")
-           (commit "e4af34b6494dc5834e4c3eeb3065fbc71afa64a1")))
+           (commit "a9f00066a5fc855c0f65b55075a956dd1ba9ef88")))
      (sha256
-      (base32 "1r2kkc9kqdp52n74dp71wmfk2iymw26bcqv7jdpzfn560rj1b2bd"))))
+      (base32 "16w44zhn5792b6fgx1hl47xsdlbwsy6sk6mvijc1av472i5xc9ij"))))
    (home-page "https://github.com/paul-jewell/paulj-dwm.git")
    (synopsis "A version of dwm, configured by paulj")))
