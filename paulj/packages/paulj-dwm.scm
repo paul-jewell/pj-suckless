9bded7ea6230bd43364d1544be194f4340ae1dbb
Paul Jewell <paul@teulu.org>
2021-02-23 14:07:49 +0000

    Added missing code

--- paulj/packages/paulj-dwm.scm
+++ paulj/packages/paulj-dwm.scm
@@ -13,8 +13,8 @@
      (method git-fetch)
      (uri (git-reference
            (url "https://github.com/paul-jewell/paulj-dwm.git")
-           (commit "27405d1effb8538c412314f277ddf809f299917b")))
+           (commit "e9c8293e8fce06e9caeee3159938eb7d515c6976")))
      (sha256
-      (base32 "1rndpi7fcpskz3s831rn1syf2957xl1aqpmd4ncxpb8scxfjirs0"))))
+      (base32 "0s92c057m3jqhx542h21a4r5hvy4gv63qjiv8p1f9la1pq75caa3"))))
    (home-page "https://github.com/paul-jewell/paulj-dwm.git")
    (synopsis "A version of dwm, configured by paulj")))
