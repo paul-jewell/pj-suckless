a0563626826d4f2a3ae7db4cdd4afa40b939e672
Paul Jewell <paul@teulu.org>
2021-02-25 11:26:30 +0000

    Renamed paulj-dwmblocks

--- paulj/packages/dwmblocks.scm
+++ paulj/packages/dwmblocks.scm
@@ -1,11 +1,11 @@
-(define-public paulj-dwmblocks
+(define-public dwmblocks
   (package
-    (name "paulj-dwmblocks")
+    (name "dwmblocks")
     (version "6.2")
     (source (origin
               (method git-fetch)
               (uri (git-reference
-                    (url "https://github.com/paul-jewell/paulj-dwmblocks.git"
+                    (url "https://github.com/paul-jewell/dwmblocks.git"
                          (commit "747067e64e64c2723c5a71f518498c2162195d45"))))
               (sha256
                (base32 "05ixpad1xh3nmcm5vfkv0x04jys56wwqsgiy41pbd6ga5d5jq88y"))))
@@ -17,16 +17,18 @@
              (string-append "PREFIX=" %output))
        #:phases
        (modify-phases %standard-phases
-         (replace 'configure
-           (lambda _
-             (substitute* "Makefile" (("\\$\\{CC\\}") "gcc"))
-             #t))
-         (replace 'install
-           (lambda* (#:key outputs #:allow-other-keys)
-             (let ((out (assoc-ref outputs "out")))
-               (invoke "make" "install"
-                       (string-append "DESTDIR=" out) "PREFIX=")))))))
-    (home-page "https://github.com/paul-jewell/paulj-dwmblocks.git")
+         (delete 'configure)) ;; No configure stage
+       ;; (modify-phases %standard-phases
+       ;;   (replace 'configure
+       ;;     (lambda _
+       ;;       (substitute* "Makefile" (("\\$\\{CC\\}") "gcc"))
+       ;;       #t))
+       ;;   (replace 'install
+       ;;     (lambda* (#:key outputs #:allow-other-keys)
+       ;;       (let ((out (assoc-ref outputs "out")))
+       ;;         (invoke "make" "install"
+       ;;                 (string-append "DESTDIR=" out) "PREFIX=")))))))
+    (home-page "https://github.com/paul-jewell/dwmblocks.git")
     (synopsis "Status bar manager for dwm")
     (description "dwmblocks is a status bar manager for dwm window manager.")
     (license license:isc)))
--- update-pkg.sh
+++ update-pkg.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh 
 
 suckless_root="/home/paul/projects/guix/pj-suckless"
-for repo in paulj-dmenu paulj-dwm paulj-slock paulj-st paulj-dwmblocks
+for repo in paulj-dmenu paulj-dwm paulj-slock paulj-st dwmblocks
     do 	
         git_hash=$(git -C ~/git/$repo log -n 1 --format=format:"%H") 	
         guix_hash=$(guix hash --hash=sha256 --format=nix-base32 -rx ~/git/$repo) 	
